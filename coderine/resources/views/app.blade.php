8522428cf57ff94a28423a670a9aae27c5d9fa70
Jesus Narbona <jesusnarbona2001@gmail.com>
2023-04-15 18:04:58 +0200

    feat(coderine): change app template

--- coderine/resources/views/app.blade.php
+++ coderine/resources/views/app.blade.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Vue 3 in Laravel 9 with Vite</title>
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>{{env('APP_NAME')}}</title>
 
 	@vite('resources/css/app.css')
 </head>
 <body>
-	<div id="app"></div>
+	@if(Auth::check())
+		<script>
+			window.Laravel = {!! json_encode([
+				'isLoggein' => true,
+				'user' => Auth::user(),
+			]) !!};
+		</script>
+	@else
+		<script>
+			window.Laravel = {!! json_encode([
+				'isLoggein' => false,
+			]) !!};
+		</script>
+	@endif
 
+	<div id="app"></div>
 	@vite('resources/js/app.js')
 </body>
 </html>
