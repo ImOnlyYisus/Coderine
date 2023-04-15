9e125b49789d52f6e7626aff8d2d61c5ff1c8e68
Jesus Narbona <jesusnarbona2001@gmail.com>
2023-04-15 17:55:20 +0200

    feat(coderine): change routes

--- coderine/routes/api.php
+++ coderine/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Router::post('login', [UserController::class, 'login']);
+Router::post('register', [UserController::class, 'register']);
+Router::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
