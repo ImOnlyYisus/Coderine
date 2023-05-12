f7308f5ad91afa7ffc60193ec65adc1ef55e0e70
Jesus Narbona <jesusnarbona2001@gmail.com>
2023-05-12 18:35:26 +0200

    feat(coderine-frontend): made a composable to use localStorage

---
+++ coderine-frontend/src/composables/storage.js
@@ -0,0 +1,22 @@
+import { encryptData, decryptData} from "../utils/encrypt-data"
+
+const saveStorage = (key, value) => {
+    const encryptedValue = encryptData(value)
+    localStorage.setItem(key, encryptedValue);
+}
+
+const getStorage = (key) => {
+    if(!localStorage.getItem(key)){
+        return null
+    }
+
+    const encryptedValue = localStorage.getItem(key)
+    const decryptedValue = decryptData(encryptedValue)
+    return decryptedValue
+}
+
+const removeStorage = (key) => {
+    localStorage.removeItem(key)
+}
+
+export { saveStorage, getStorage, removeStorage }
