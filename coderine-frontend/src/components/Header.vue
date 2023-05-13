7f0253e2c66767e52f7d0b2077f1a20d28c5f3bb
Jesus Narbona <jesusnarbona2001@gmail.com>
2023-05-13 15:48:14 +0200

    feat(coderine-frontend): header change when scroll

--- coderine-frontend/src/components/Header.vue
+++ coderine-frontend/src/components/Header.vue
@@ -1,64 +1,36 @@
 <template>
     <header class="generic__header">
-        <div class="header__container">
+        <div class="header__container" :class="{ 'scroll': isShow }">
             <div class="header__nav-links--left">
-                <RouterLink to="/"
-                    ><strong>01.</strong>{{ $t('header-component.links.home') }}</RouterLink
-                >
-                <RouterLink to="/playground"
-                    ><strong>02.</strong>{{ $t('header-component.links.playground') }}</RouterLink
-                >
-                <RouterLink to="/components"
-                    ><strong>03.</strong>{{ $t('header-component.links.components') }}</RouterLink
-                >
+                <RouterLink to="/"><strong>01.</strong>{{ $t('header-component.links.home') }}</RouterLink>
+                <RouterLink to="/playground"><strong>02.</strong>{{ $t('header-component.links.playground') }}</RouterLink>
+                <RouterLink to="/components"><strong>03.</strong>{{ $t('header-component.links.components') }}</RouterLink>
             </div>
 
             <img src="/images/logo-dark.png" alt="Logo Coderine" class="header__logo" />
             <div class="header__nav-links-right">
-                <RouterLink
-                    to="/register"
-                    v-if="!useAuthStore().isAutheticated"
-                    class="generic__button--primary"
-                    >{{ $t('header-component.links.register') }}
+                <RouterLink to="/register" v-if="!useAuthStore().isAutheticated" class="generic__button--primary">{{
+                    $t('header-component.links.register') }}
                 </RouterLink>
-                <RouterLink
-                    to="/login"
-                    v-if="!useAuthStore().isAutheticated"
-                    class="generic__button--secondary"
-                    >{{ $t('header-component.links.login') }}
+                <RouterLink to="/login" v-if="!useAuthStore().isAutheticated" class="generic__button--secondary">{{
+                    $t('header-component.links.login') }}
                 </RouterLink>
-                <span
-                    v-if="useAuthStore().isAutheticated"
-                    class="generic__button--secondary"
-                    @click="onLogout"
-                    >{{ $t('header-component.links.logout') }}
+                <span v-if="useAuthStore().isAutheticated" class="generic__button--secondary" @click="onLogout">{{
+                    $t('header-component.links.logout') }}
                 </span>
 
                 <div class="header__user" v-if="useAuthStore().isAutheticated">
-                    <img
-                        :src="useAuthStore().user.avatar"
-                        alt="Avatar"
-                        class="header__user--avatar"
-                        :title="useAuthStore().user.name"
-                    />
+                    <img :src="useAuthStore().user.avatar" alt="Avatar" class="header__user--avatar"
+                        :title="useAuthStore().user.name" />
                     <p class="header__user--name">{{ useAuthStore().user.name }}</p>
                 </div>
 
                 <div class="header__language-selector">
                     <div class="header__language-selector--button">
-                        <input
-                            type="checkbox"
-                            class="header__language-selector--checkbox"
-                            @click="onToggleLanguage"
-                            v-if="isChecked"
-                        />
-                        <input
-                            type="checkbox"
-                            class="header__language-selector--checkbox"
-                            @click="onToggleLanguage"
-                            v-else
-                            checked
-                        />
+                        <input type="checkbox" class="header__language-selector--checkbox" @click="onToggleLanguage"
+                            v-if="isChecked" />
+                        <input type="checkbox" class="header__language-selector--checkbox" @click="onToggleLanguage" v-else
+                            checked />
                         <div class="header__language-selector--knobs"></div>
                         <div class="header__language-selector--layer"></div>
                     </div>
@@ -75,8 +47,13 @@ import { computed } from 'vue';
 import { useAuthStore } from '@/stores/auth';
 import { confirmCancel } from '@/utils/sweetAlerts';
 import { toastify } from '@/utils/toastify';
+import { useWindowScroll } from '@vueuse/core';
+
+const { y: scrollY } = useWindowScroll();
+const isShow = computed(() => scrollY.value > 50);
 
 const { t, locale } = useI18n();
+
 
 const onToggleLanguage = () => {
     const supportedLocales = ['es', 'en'];
@@ -100,4 +77,6 @@ const onLogout = () => {
 const isChecked = computed(() => {
     return locale.value === 'es';
 });
+
+
 </script>
